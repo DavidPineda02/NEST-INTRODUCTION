40bdf860bde8bad67995523ea225d8eac4edd6ad
DavidPineda02 <karertian2016@gmail.com>
2023-08-25 06:49:21 -0500

    feat: :sparkles: Agregando un Modulo en Nest
    Agregando el modulo de Astronautas desde la terminal

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ParachutesModule } from './parachutes/parachutes.module';
+import { AstronautasModule } from './astronautas/astronautas.module';
 
 @Module({
-  imports: [ParachutesModule,MongooseModule.forRoot('mongodb+srv://AstronautasUser:12345@astronautascluster.llkcsil.mongodb.net/astronautasDB')],
+  imports: [ParachutesModule,MongooseModule.forRoot('mongodb+srv://AstronautasUser:12345@astronautascluster.llkcsil.mongodb.net/astronautasDB'), AstronautasModule],
   controllers: [AppController],
   providers: [AppService],
 })
