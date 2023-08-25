c7e478ed67dbe855e8623e3122369fd57140bafb
DavidPineda02 <karertian2016@gmail.com>
2023-08-25 06:47:22 -0500

    feat: :zap: Conexion con la base de datos
    Conectamos el trabajo con Mongoose Compass

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ParachutesModule } from './parachutes/parachutes.module';
 
 @Module({
-  imports: [ParachutesModule],
+  imports: [ParachutesModule,MongooseModule.forRoot('mongodb+srv://AstronautasUser:12345@astronautascluster.llkcsil.mongodb.net/astronautasDB')],
   controllers: [AppController],
   providers: [AppService],
 })
