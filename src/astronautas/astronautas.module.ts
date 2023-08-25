defda2b50a6e10dbc7ae2d233d21aac9495257c0
DavidPineda02 <karertian2016@gmail.com>
2023-08-25 07:11:39 -0500

    feat: :sparkles: Haciendo el Schema y Agregando el Servicio
    Se hizo el schema de Astronauta y se agrego desde terminal el servicio de Astronauta

--- src/astronautas/astronautas.module.ts
+++ src/astronautas/astronautas.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { Astronauta, AstronautaSchema } from './schemas/astronautas.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AstronautasService } from './astronautas.service';
 
-@Module({})
+@Module({
+    imports:[
+        MongooseModule.forFeature([{
+            name: Astronauta.name,
+            schema: AstronautaSchema
+        }]) 
+    ],
+    
+    providers: [AstronautasService]
+})
+
 export class AstronautasModule {}
